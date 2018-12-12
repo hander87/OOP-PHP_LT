0c8422d5bbfffdf190ba6568e556c649dce5285a
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-12-12 14:49:15 +0100

    Abstract class added

---
+++ BankAccount.php
@@ -0,0 +1,89 @@
+<?php
+
+abstract class BankAccount {
+
+    // Variables
+    protected $balance = 500; // protected get black boxed, but accessible by other classes
+    public $apr;
+    public $sortCode;
+    public $firstName;
+    public $lastName;
+    public $audit = array();
+    protected $locked = false;
+
+    // Methods
+    public function withdraw( $amount ) {
+        $transDate = new DateTime();
+
+        if ( $this->locked === false ) {
+            $this->balance -= $amount;
+
+            array_push( $this->audit, 
+                array( 
+                    "Withdraw accepted!",
+                    $amount,
+                    $this->balance,
+                    $transDate->format( 'c') 
+                ) 
+            );
+        } else {
+            array_push( $this->audit, 
+                array( 
+                    "Withdraw denied!",
+                    $amount,
+                    $this->balance,
+                    $transDate->format( 'c') 
+                ) 
+            );
+        }
+    }
+
+    public function deposit( $amount ) {
+        $transDate = new DateTime();
+
+        if ( $this->locked === false ) {
+            $this->balance += $amount;
+
+            array_push( $this->audit, 
+                array( 
+                    "Deposit accepted!",
+                    $amount,
+                    $this->balance,
+                    $transDate->format( 'c') 
+                ) 
+            );
+        } else {
+            array_push( $this->audit, 
+                array( 
+                    "Deposit denied!",
+                    $amount,
+                    $this->balance,
+                    $transDate->format( 'c') 
+                ) 
+            );
+        }
+    }
+
+    public function lock () {
+        $this->locked = true;
+        $lockedDate = new DateTime();
+        array_push( $this->audit, 
+            array( 
+                "Account locked!",
+                $lockedDate->format( 'c') 
+            ) 
+        );
+    }
+
+    public function unlock () {
+        $this->locked = false;
+        $unlockedDate = new DateTime();
+        array_push( $this->audit, 
+            array( 
+                "Account unlocked!",
+                $unlockedDate->format( 'c') 
+            ) 
+        );
+    }
+
+}
